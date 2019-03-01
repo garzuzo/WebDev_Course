e8a1b848cf24051ebfb13e2524cf80b1e0225d6d
garzuzo <garzuxo@gmail.com>
2019-03-01 00:15:27 -0500

    firebase

--- angcli/src/app/app-routing.module.ts
+++ angcli/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { LibroComponent } from './libro/libro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:"inicio", component:InicioComponent},
+  {path:"libro", component:LibroComponent},
+  {path:"libromod", loadChildren:"./modules/libro/libro.module#LibroModule"},
+  {path:"",redirectTo:"inicio",pathMatch:"full"},
+  {path:"**",redirectTo:"inicio",pathMatch:"full"},
+  //el # es para que escoja ese componente por defecto
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angcli/src/app/models/libro.ts
+++ angcli/src/app/models/libro.ts
@@ -1,6 +1,6 @@
 export class Libro{
 
-    id:number;
+    id:string;
     name:string;
     author:string;
 
